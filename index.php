021e1bbc4f0ceeb239e160e90be08bd0ebe720b3
Karan Purohit <karnpurohit@gmailcom>
2015-08-16 16:40:57 +0530

    very basic registration system

--- index.php
+++ index.php
@@ -10,11 +10,14 @@
 <body>
 <form action=/php/login.php method="post">
     User Name: <input type="text" name="name"><br>
-    <? echo $err_username; //error username invalid?>
+    <? echo isset($_POST["err_name"])? "This field is required" : "";?>
     Password: <input type="password" name="password"><br>
-    <? echo $err_password; //error username invalid?>
+    <? echo isset($_POST["err_password"])? "This field is required" : "";?>
     <input type="submit">
-</form>
 
+    <?
+    //Todo: Add cleanup for the $_Post error messages
+    ?>
+</form>
 </body>
 </html>
---
+++ php/register.php
@@ -0,0 +1,48 @@
+<?
+    /*
+    *   This file contains the php script for registration process
+    */
+
+    $servername = "127.0.0.1";
+    $username = "pagesuser";
+    $password = "password";
+
+    // Create connection
+    $mysqli = new mysqli($servername, $username, $password,'ProjectPages');
+
+    // Check connection
+    if ($mysqli->connect_errno) {
+        printf("Connect failed: %s\n", $mysqli->connect_error);
+        exit();
+    }
+
+    //Check if method is POST
+    if ($_SERVER["REQUEST_METHOD"] == "POST")
+      {
+          $user_name =$_POST['user_name'];
+          $first_name=$_POST['first_name'];
+          $last_name=$_POST['last_name'];
+          $password=$_POST['password'];
+          $email=$_POST['email'];
+
+        $sql = "Insert into users (user_name,first_name,last_name,password,email) values
+        (
+            '$user_name',
+            '$first_name',
+            '$last_name',
+            '$password',
+            '$email'
+        );";
+
+        if ($mysqli->query($sql) == TRUE) {
+            echo "New record created successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . $mysq->error;
+        }
+      }
+    $mysqli->close();
+
+    //if session is set, re direst to homepage;
+    require("./redirect_header.php");
+
+?>
