0d0ae01b42b60d94ff85e52c06802aa9ea7f63e1
Karan Purohit <karnpurohit@gmailcom>
2015-08-22 12:19:27 +0530

    few login bugs solved

--- redirect_header.php
+++ redirect_header.php
@@ -1,15 +1,11 @@
 <?
     //Code to redirect user when ever the user is already logged in;
     if ((isset($_SESSION['user']))){
-        if((strcmp(basename(__FILE__),"home.php"))){
             header("Location: ./home.php");
             die();
-        }
     }
     else{
-        if((strcmp(basename(__FILE__),"index.php")))
-        {
             header("Location: ./index.php");
-        }
+            die("boom");
     }
 ?>
