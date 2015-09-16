aad4aa92593d85409a2ed14a7858a42a033a1baa
Karan Purohit <karnpurohit@gmailcom>
2015-09-16 09:01:56 +0530

    added fevicon

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
 	<head>
 	    <?
@@ -6,6 +7,7 @@
 	            header("Location: ./home.php");
 	        }
 		?>
+		<link href="favicon.png" rel="icon" type="image/png" />
 		<title>Welcome to Pages</title>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
